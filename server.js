72467f66e9ce23f4811ee3ba88907f330eb46d62
Brandon-Schefstad <bschefstad@gmail.com>
2022-07-27 18:09:21 -0400

    Trying API Call

--- server.js
+++ server.js
@@ -54,13 +54,10 @@ app.delete('/deleteShower', (request, response) => {
 
 app.get('/api/:name', (request, response) => {
 	const showerName = request.params.name.toLowerCase();
-	console.log(showerName);
-	db.collection('meteor-showers')
-		.find({ name: showerName })
-		.toArray()
-		.then((data) => {
-			response.json(data);
-		});
+	db.collection('meteor-showers').find({ name: showerName }).toArray();
+	// .then((data) => {
+	// 	response.json(data);
+	// });
 });
 app.listen(process.env.PORT || PORT, () => {
 	console.log(`Server running on port ${PORT}`);
