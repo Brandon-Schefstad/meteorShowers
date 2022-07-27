baa1233415659a34881b75385f64b6cb35cb0d17
Brandon-Schefstad <bschefstad@gmail.com>
2022-07-27 17:20:51 -0400

    Fix?

--- views/client.js
+++ views/client.js
@@ -3,27 +3,8 @@ const deleteButton = document.querySelectorAll('.fa-circle-minus');
 Array.from(deleteButton).forEach((button) => {
 	button.addEventListener('click', deleteShower);
 });
-async function test() {
-	const name = this.parentNode.childNodes[1].childNodes[0].data;
-	console.log(name);
-}
+
 async function deleteShower() {
 	const name = this.parentNode.childNodes[1].childNodes[0].data;
 	console.log(name);
-	try {
-		const response = await fetch('/deleteShower', {
-			method: 'delete',
-			// body: JSON.stringify({
-			// 	name: name,
-			// }),
-		});
-		// console.log(request);
-		// const data = await request.json();
-		// console.log(data);
-		// location.reload();
-	} catch (error) {
-		console.log(error);
-	}
 }
-
-console.log('test');
